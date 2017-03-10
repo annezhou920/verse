ffedd13b4c452c057c34c46814e24b82be1789ea
Anne Zhou <anne.zhou920@gmail.com>
2017-03-10 18:03:45 -0500

    need to fix picker

--- app/Verse.js
+++ app/Verse.js
@@ -5,10 +5,16 @@ import {
   Text,
   View,
   ScrollView,
-  TouchableHighlight
+  TouchableHighlight,
+  Picker
 } from 'react-native';
 import poems from './poems.js'
 import TabBar from './TabBar'
+
+const Item = Picker.Item;
+const googleBaseUrl =
+  "https://translation.googleapis.com/language/translate/v2?"
+const apiKey = "key=AIzaSyA7MUv6j75JssJAITGWYsN2vZh6AVLRtVI&"
 
 
 class VerseScreen extends Component {
@@ -18,13 +24,11 @@ class VerseScreen extends Component {
   constructor(){
     super()
     this.state = {
-      poem: {
-        title: poems[0].title,
-        poet: poems[0].poet,
-        verse: poems[0].verse
-      }
+      selectedLang: poems[0].language,
+      poem: poems[0]
     }
     this.fetchPoem = this.fetchPoem.bind(this)
+    this.translateText = this.translateText.bind(this)
   }
 
   componentDidMount(){
@@ -36,43 +40,108 @@ class VerseScreen extends Component {
     let max = poems.length - 1
     let randomPoemNum = Math.floor(Math.random() * (max - min + 1)) + min;
     this.setState({
+      selectedLang: poems[randomPoemNum].language,
       poem: {
         title: poems[randomPoemNum].title,
         poet: poems[randomPoemNum].poet,
-        verse: poems[randomPoemNum].verse
+        language: poems[randomPoemNum].language,
+        // verse: poems[randomPoemNum].verse
+        lines: poems[randomPoemNum].lines
       }
     })
   }
 
+  translateText(key, newLang){
+    this.setState({ selectedLang: newLang })
+
+    let currentPoem = this.state.poem
+    let baseUrl = googleBaseUrl + apiKey
+    let currentLang = "source=" + currentPoem.language + "&"
+    let targetLang = "target=" + newLang + "&q="
+    let lines = currentPoem.lines
+    let poet = currentPoem.poet
+    let title = currentPoem.title
+    let queryString = lines.join("&q=") + "&q=" + title
+    let fetchUrl = baseUrl + currentLang + targetLang + queryString
+
+    fetch(fetchUrl, {method: "GET"})
+        .then((res) => res.json())
+        .then((resData) => {
+          translatedText = []
+          resData.data.translations.forEach(function(translation){
+            translatedText.push(translation.translatedText)
+          })
+          this.setState({
+            poem: {
+              language: newLang,
+              title: translatedText.pop(),
+              poet: poet,
+              lines: translatedText
+            }
+          })
+
+        })
+        .done();
+  }
+
+  // renderLoading(){
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text>
+  //         Translating poem from {this.state.poem.language} to {this.state.selectedLang}
+  //       </Text>
+  //     </View>
+  //   )
+  // }
+  //
+  // renderPoem(){
+  //   return (
+  //
+  //   )
+  // }
+
   render() {
+
     return (
-      <View style={styles.container}>
+
+      <View style={styles.poemContainer}>
+      <Picker
+        selectedValue={this.state.selectedLang}
+        onValueChange={this.translateText.bind(this, 'selectedLang')}
+        mode="dropdown">
+        <Item label="English"   value="en" />
+        <Item label="Spanish"   value="es" />
+        <Item label="Chinese"   value="zh-CN" />
+        <Item label="Icelandic" value="is" />
+        <Item label="Arabic"    value="ar" />
+      </Picker>
+
+
         <ScrollView>
           <Text style={styles.title}>
-
             {this.state.poem.title}
           </Text>
-          <Text style={styles.author}>
 
+          <Text style={styles.author}>
             {this.state.poem.poet}
           </Text>
-          <Text style={styles.text}>
 
-            {this.state.poem.verse}
+          <Text style={styles.text}>
+            {this.state.poem.lines.join("\n")}
           </Text>
         </ScrollView>
+
 
          <View style={styles.buttonContainer}>
           <TouchableHighlight
             style={styles.button}
-            underlayColor='#ccc'
+            underlayColor='transparent'
             onPress={this.fetchPoem}>
             <Text style={styles.buttonText}>Shuffle</Text>
           </TouchableHighlight>
         </View>
 
-      </View>
-
+    </View>
 
 
     );
@@ -80,36 +149,50 @@ class VerseScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+
+  poemContainer: {
     flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'darkseagreen'
+  },
+  picker: {
+    // flex: 0,
+    width: 100,
+    color: 'white'
   },
   title: {
-    marginTop: 50,
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: 'left',
     margin: 5,
+    marginLeft: 50,
+    color: 'white'
   },
   author: {
     fontSize: 15,
-    textAlign: 'center',
-    // margin: 5,
+    marginLeft: 50,
+    textAlign: 'left',
+    color: 'white',
+    paddingBottom: 10
   },
   text: {
     textAlign: 'left',
     color: '#333333',
     marginBottom: 50,
+    marginLeft: 50,
+    color: 'white',
+    marginRight: 50,
   },
   buttonContainer: {
     height: 50,
-    backgroundColor: '#eee',
+    marginTop: 20,
+    backgroundColor: 'olive',
     justifyContent: 'center',
     alignItems: 'center'
   },
   button: {
-    backgroundColor: '#eee',
+    // backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -117,7 +200,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    color: '#666666',
+    color: 'white',
   },
 });
 
--- app/poems.js
+++ app/poems.js
@@ -1,219 +1,230 @@
+
 const poemData = [
-    {
-      "id": 1,
-      "poet": "Audre Lorde",
-      "title": "A Woman Speaks",
-      "verse": `
-                Moon marked and touched by sun
-                my magic  is unwritten
-                but when the sea turns back
-                it will leave my shape behind.
-                I seek no favor
-                untouched by blood
-                unrelenting as the curse of love
-                permanent as my errors
-                or my pride
-                I do not mix
-                love with pity
-                nor hate with scorn
-                and if you would know me
-                look into the entrails of Uranus
-                where the restless oceans pound.
-
-                I do not dwell
-                within my birth nor my divinities
-                who am ageless and half-grown
-                and still seeking
-                my sisters
-                witches in Dahomey
-                wear me inside their coiled cloths
-                as our mother did
-                mourning.
-
-                I have been woman
-                for a long time
-                beware my smile
-                I am treacherous with old magic
-                and the noon's new fury
-                with all your wide futures
-                promised
-                I am
-                woman
-                and not white.`
-    },
-    {
-      "id": 2,
-      "poet": "Carl Phillips",
-      "title": "Blue",
-      "verse": `
-                As through marble or the lining of
-                certain fish split open and scooped
-                clean, this is the blue vein
-                that rides, where the flesh is even
-                whiter than the rest of her, the splayed
-                thighs mother forgets, busy struggling
-                for command over bones: her own,
-                those of the chaise longue, all
-                equally uncooperative, and there’s
-                the wind, too. This is her hair, gone
-                from white to blue in the air.
-
-                This is the black, shot with blue, of my dark
-                daddy’s knuckles, that do not change, ever.
-                Which is to say they are no more pale
-                in anger than at rest, or when, as
-                I imagine them now, they follow
-                the same two fingers he has always used
-                to make the rim of every empty blue
-                glass in the house sing.
-                Always, the same
-                blue-to-black sorrow
-                no black surface can entirely hide.
-
-                Under the night, somewhere
-                between the white that is nothing so much as
-                blue, and the black that is, finally; nothing,
-                I am the man neither of you remembers.
-                Shielding, in the half-dark,
-                the blue eyes I sometimes forget
-                I don’t have. Pulling my own stoop-
-                shouldered kind of blues across paper.
-                Apparently misinformed about the rumored
-                stuff of dreams: everywhere I inquired,
-                I was told look for blue.`
-    },
-    {
-      "id": 3,
-      "poet": "Warsan Shire",
-      "title": "For Women Who Are Difficult to Love",
-      "verse": `
-              you are a horse running alone
-              and he tries to tame you
-              compares you to an impossible highway
-              to a burning house
-              says you are blinding him
-              that he could never leave you
-              forget you
-              want anything but you
-              you dizzy him, you are unbearable
-              every woman before or after you
-              is doused in your name
-              you fill his mouth
-              his teeth ache with memory of taste
-              his body just a long shadow seeking yours
-              but you are always too intense
-              frightening in the way you want him
-              unashamed and sacrificial
-              he tells you that no man can live
-              up to the one who
-              lives in your head
-              and you tried to change didn't you?
-              closed your mouth more
-              tried to be softer
-              prettier
-              less volatile, less awake
-              but even when sleeping you could feel
-              him travelling away from you in his dreams
-              so what did you want to do love
-              split his head open?
-              you can't make homes out of human beings
-              someone should have already told you that
-              and if he wants to leave
-              then let him leave
-              you are terrifying
-              and strange and beautiful
-              something not everyone knows how to love.`
-    },
-    {
-      "id": 4,
-      "poet": "Fatimah Asghar",
-      "title": "If They Should Come for Us",
-      "verse": `
-              these are my people & I find
-              them on the street & shadow
-              through any wild all wild
-              my people my people
-              a dance of strangers in my blood
-              the old woman’s sari dissolving to wind
-              bindi a new moon on her forehead
-              I claim her my kin & sew
-              the star of her to my breast
-              the toddler dangling from stroller
-              hair a fountain of dandelion seed
-              at the bakery I claim them too
-              the sikh uncle at the airport
-              who apologizes for the pat
-              down the muslim man who abandons
-              his car at the traffic light drops
-              to his knees at the call of the azan
-              & the muslim man who sips
-              good whiskey at the start of maghrib
-              the lone khala at the park
-              pairing her kurta with crocs
-              my people my people I can’t be lost
-              when I see you my compass
-              is brown & gold & blood
-              my compass a muslim teenager
-              snapback & high-tops gracing
-              the subway platform
-              mashallah I claim them all
-              my country is made
-              in my people’s image
-              if they come for you they
-              come for me too in the dead
-              of winter a flock of
-              aunties step out on the sand
-              their dupattas turn to ocean
-              a colony of uncles grind their palms
-              & a thousand jasmines bell the air
-              my people I follow you like constellations
-              we hear the glass smashing the street
-              & the nights opening their dark
-              our names this country’s wood
-              for the fire my people my people
-              the long years we’ve survived the long
-              years yet to come I see you map
-              my sky the light your lantern long
-              ahead & I follow I follow`
-    },
-    {
-      "id": 5,
-      "poet": "Mai Der Vang",
-      "title": "Last Body",
-      "verse": `
-              I can’t leave my hurting skull
-              Or the rose apple opening inside me.
-
-              I’ll count the weeks, months,
-              Unfurling each numbered day in my hair.
-
-              Frost ribbons inside my brain,
-              Canals push up my leg.
-
-              I’m moving on
-              To what the world needs me to know.
-
-              I am the angel trapped inside the bullet.
-              I am the exit wound trapped inside the angel.
-
-              Am I the scarecrow
-              Perched at the end of the human trail.
-
-              I’ll palm cotton between my prayers
-              Until the universe has passed,
-
-              Waving down jellyfish
-              To volcano hours.
-
-              What force propels a bullet
-              From its chamber. Is it sourced by water
-
-              Trickling in a karst cave,
-              Or is it an angel’s gasp as she flees.
-
-              I can’t answer it all,
-              But my mask grows taller every year.`
-    },
-  ]
+  {
+    "id": 1,
+    "language": "en",
+    "poet": "Audre Lorde",
+    "title": "A Woman Speaks",
+    "lines" : [
+      "Moon marked and touched by sun",
+      "my magic is unwritten",
+      "but when the sea turns back",
+      "it will leave my shape behind.",
+      "I seek no favor",
+      "untouched by blood",
+      "unrelenting as the curse of love",
+      "permanent as my errors",
+      "or my pride",
+      "I do not mix",
+      "love with pity",
+      "nor hate with scorn",
+      "and if you would know me",
+      "look into the entrails of Uranus",
+      "where the restless oceans pound.",
+      "",
+      "I do not dwell",
+      "within my birth nor my divinities",
+      "who am ageless and half-grown",
+      "and still seeking",
+      "my sisters",
+      "witches in Dahomey",
+      "wear me inside their coiled cloths",
+      "as our mother did",
+      "mourning.",
+      "",
+      "I have been woman",
+      "for a long time",
+      "beware my smile",
+      "I am treacherous with old magic",
+      "and the noon's new fury",
+      "with all your wide futures",
+      "promised",
+      "I am",
+      "woman",
+      "and not white."
+    ]
+  },
+  {
+    "id": 2,
+    "language": "en",
+    "poet": "Carl Phillips",
+    "title": "Blue",
+    "lines" : [
+      "As through marble or the lining of",
+      "certain fish split open and scooped",
+      "clean, this is the blue vein",
+      "that rides, where the flesh is even",
+      "whiter than the rest of her, the splayed",
+      "thighs mother forgets, busy struggling",
+      "for command over bones: her own,",
+      "those of the chaise longue, all",
+      "equally uncooperative, and there’s",
+      "the wind, too. This is her hair, gone",
+      "from white to blue in the air.",
+      "",
+      "This is the black, shot with blue, of my dark",
+      "daddy’s knuckles, that do not change, ever.",
+      "Which is to say they are no more pale",
+      "in anger than at rest, or when, as",
+      "I imagine them now, they follow",
+      "the same two fingers he has always used",
+      "to make the rim of every empty blue",
+      "glass in the house sing.",
+      "Always, the same",
+      "blue-to-black sorrow",
+      "no black surface can entirely hide.",
+      "",
+      "Under the night, somewhere",
+      "between the white that is nothing so much as",
+      "blue, and the black that is, finally; nothing,",
+      "I am the man neither of you remembers.",
+      "Shielding, in the half-dark,",
+      "the blue eyes I sometimes forget",
+      "I don’t have. Pulling my own stoop-",
+      "shouldered kind of blues across paper.",
+      "Apparently misinformed about the rumored",
+      "stuff of dreams: everywhere I inquired,",
+      "I was told look for blue."
+    ]
+  },
+  {
+    "id": 3,
+    "language": "en",
+    "poet": "Warsan Shire",
+    "title": "For Women Who Are Difficult to Love",
+    "lines": [
+      "you are a horse running alone",
+      "and he tries to tame you",
+      "compares you to an impossible highway",
+      "to a burning house",
+      "says you are blinding him",
+      "that he could never leave you",
+      "forget you",
+      "want anything but you",
+      "you dizzy him, you are unbearable",
+      "every woman before or after you",
+      "is doused in your name",
+      "you fill his mouth",
+      "his teeth ache with memory of taste",
+      "his body just a long shadow seeking yours",
+      "but you are always too intense",
+      "frightening in the way you want him",
+      "unashamed and sacrificial",
+      "he tells you that no man can live",
+      "up to the one who",
+      "lives in your head",
+      "and you tried to change didn't you?",
+      "closed your mouth more",
+      "tried to be softer",
+      "prettier",
+      "less volatile, less awake",
+      "but even when sleeping you could feel",
+      "him travelling away from you in his dreams",
+      "so what did you want to do love",
+      "split his head open?",
+      "you can't make homes out of human beings",
+      "someone should have already told you that",
+      "and if he wants to leave",
+      "then let him leave",
+      "you are terrifying",
+      "and strange and beautiful",
+      "something not everyone knows how to love."
+    ]
+  },
+  {
+    "id": 4,
+    "language": "en",
+    "poet": "Fatimah Asghar",
+    "title": "If They Should Come for Us",
+    "lines": [
+        "these are my people and I find",
+        "them on the street and shadow",
+        "through any wild all wild",
+        "my people my people",
+        "a dance of strangers in my blood",
+        "the old woman’s sari dissolving to wind",
+        "bindi a new moon on her forehead",
+        "I claim her my kin and sew",
+        "the star of her to my breast",
+        "the toddler dangling from stroller",
+        "hair a fountain of dandelion seed",
+        "at the bakery I claim them too",
+        "the sikh uncle at the airport",
+        "who apologizes for the pat",
+        "down the muslim man who abandons",
+        "his car at the traffic light drops",
+        "to his knees at the call of the azan",
+        "and the muslim man who sips",
+        "good whiskey at the start of maghrib",
+        "the lone khala at the park",
+        "pairing her kurta with crocs",
+        "my people my people I can’t be lost",
+        "when I see you my compass",
+        "is brown and gold and blood",
+        "my compass a muslim teenager",
+        "snapback and high-tops gracing",
+        "the subway platform",
+        "mashallah I claim them all",
+        "my country is made",
+        "in my people’s image",
+        "if they come for you they",
+        "come for me too in the dead",
+        "of winter a flock of",
+        "aunties step out on the sand",
+        "their dupattas turn to ocean",
+        "a colony of uncles grind their palms",
+        "and a thousand jasmines bell the air",
+        "my people I follow you like constellations",
+        "we hear the glass smashing the street",
+        "and the nights opening their dark",
+        "our names this country’s wood",
+        "for the fire my people my people",
+        "the long years we’ve survived the long",
+        "years yet to come I see you map",
+        "my sky the light your lantern long",
+        "ahead and I follow I follow"
+    ]
+  },
+  {
+    "id": 5,
+    "language": "en",
+    "poet": "Mai Der Vang",
+    "title": "Last Body",
+    "lines": [
+      "I can’t leave my hurting skull",
+      "Or the rose apple opening inside me.",
+      "",
+      "I’ll count the weeks, months,",
+      "Unfurling each numbered day in my hair.",
+      "",
+      "Frost ribbons inside my brain,",
+      "Canals push up my leg.",
+      "",
+      "I’m moving on",
+      "To what the world needs me to know.",
+      "",
+      "I am the angel trapped inside the bullet.",
+      "I am the exit wound trapped inside the angel.",
+      "",
+      "Am I the scarecrow",
+      "Perched at the end of the human trail.",
+      "",
+      "I’ll palm cotton between my prayers",
+      "Until the universe has passed,",
+      "",
+      "Waving down jellyfish",
+      "To volcano hours.",
+      "",
+      "What force propels a bullet",
+      "From its chamber. Is it sourced by water",
+      "",
+      "Trickling in a karst cave,",
+      "Or is it an angel’s gasp as she flees.",
+      "",
+      "I can’t answer it all,",
+      "But my mask grows taller every year."
+    ]
+  },
+]
 
 export default poemData
